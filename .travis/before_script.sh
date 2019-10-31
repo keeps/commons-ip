6cd26ddde227e76dce28cde6fdc2088ff1db5bf5
Hélder Silva <hsilva@keep.pt>
2019-10-31 15:22:11 +0000

    Updated travis related info (updated settings.xml to deploy to artifactory)

--- .travis/before_script.sh
+++ .travis/before_script.sh
@@ -2,5 +2,15 @@
 
 set -ex
 
+# How to update settings.xml.enc (to update artifactory token)
+# 1) create a copy of file temp.settings.xml (don't add it to git)
+#    > cp temp.settings.xml settings.xml
+# 2) generate new artifactory token for a user called $DATE_travis_commons_ip_deployer (e.g. 20191031_travis_commons_ip_deployer)
+#    > ask KEEPS sysadmins to do it
+# 3) encrypt file using travis tools
+#    > travis encrypt-file settings.xml (say yes to override)
+# 4) remove temp. file
+#    > rm settings.xml
+
 # decrypt maven setting.xml
 openssl aes-256-cbc -K $encrypted_f647f4e729e9_key -iv $encrypted_f647f4e729e9_iv -in .travis/settings.xml.enc -out settings.xml -d
