68686d5df337b582cc013d5d0ba0f58090c3c40a
João Gomes <jgomes@keep.pt>
2022-06-06 10:09:09 +0100

    fix checkstyle issues in main

--- src/main/java/org/roda_project/commons_ip2/validator/CLI/Main.java
+++ src/main/java/org/roda_project/commons_ip2/validator/CLI/Main.java
@@ -1,13 +1,17 @@
 package org.roda_project.commons_ip2.validator.CLI;
 
-import org.roda_project.commons_ip2.validator.utils.CLIUtils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.roda_project.commons_ip2.validator.utils.CLIUtils;
+
 /** {@author João Gomes <jgomes@keep.pt>}. */
-public class Main {
+public final class Main {
+
+  private Main() {
+    // do nothing
+  }
 
   /**
    * Main of the CLI.
@@ -15,20 +19,20 @@ public class Main {
    * @param args
    *          {@link String} array with the args of the command.
    */
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     if (args.length == 0) {
       CLIUtils.printUsage(System.out);
     } else {
       if (args[0].equals(CLIConstants.CLI_OPTION_VALIDATE)) {
-        List<String> filteredArgs = new ArrayList<>(Arrays.asList(args));
+        final List<String> filteredArgs = new ArrayList<>(Arrays.asList(args));
         filteredArgs.remove(0);
-        CLIValidator cliValidator = new CLIValidator();
+        final CLIValidator cliValidator = new CLIValidator();
         cliValidator.start(filteredArgs.toArray(new String[] {}));
       }
       if (args[0].equals(CLIConstants.CLI_OPTION_CREATE)) {
-        List<String> filteredArgs = new ArrayList<>(Arrays.asList(args));
+        final List<String> filteredArgs = new ArrayList<>(Arrays.asList(args));
         filteredArgs.remove(0);
-        CLICreator cliCreator = new CLICreator();
+        final CLICreator cliCreator = new CLICreator();
         cliCreator.start(filteredArgs.toArray(new String[] {}));
       } else {
         CLIUtils.printUsage(System.out);
