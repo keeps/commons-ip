d4df5d00fbe1e2ee401636e3d0e0b3e2c8f13250
Carlos Afonso <cafonso@keep.pt>
2024-11-28 15:45:58 +0000

    Dependencies for CITS ERMS

---
+++ src/main/java/org/roda_project/commons_ip2/cli/model/enums/ReportTypeEnums.java
@@ -0,0 +1,21 @@
+package org.roda_project.commons_ip2.cli.model.enums;
+
+/**
+ * @author Miguel Guimarães <mguimaraes@keep.pt>
+ */
+public class ReportTypeEnums {
+  public enum ReportType {
+    COMMONS_IP("commons-ip"), PYIP("eark-validator");
+
+    private final String type;
+
+    ReportType(String type) {
+      this.type = type;
+    }
+
+    @Override
+    public String toString() {
+      return type;
+    }
+  }
+}
