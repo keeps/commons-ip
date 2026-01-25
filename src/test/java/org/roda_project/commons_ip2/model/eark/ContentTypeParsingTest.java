6d79fe78bf95df5f1034cf37fe69e50ca62ca6ad
Thomas Edvardsen <thomas.edvardsen@nb.no>
2026-01-25 01:35:39 +0100

    Fixing Check Style errors

--- src/test/java/org/roda_project/commons_ip2/model/eark/ContentTypeParsingTest.java
+++ src/test/java/org/roda_project/commons_ip2/model/eark/ContentTypeParsingTest.java
@@ -7,125 +7,155 @@ import org.roda_project.commons_ip2.mets_v1_12.beans.Mets;
 import org.roda_project.commons_ip2.mets_v1_12.beans.MetsType;
 import org.roda_project.commons_ip2.model.SIP;
 
+/**
+ * Tests content type parsing from METS attributes.
+ */
 public class ContentTypeParsingTest {
 
+  private static final String SIP_TYPE = "SIP";
+  private static final String SIP_VERSION = "2.1.0";
+  private static final String TYPE_TEXT = "Text";
+  private static final String TYPE_OTHER = "Other";
+  private static final String TYPE_OTHER_UPPER = "OTHER";
+  private static final String OTHER_TYPE_VALUE = "Moving images - on tangible media";
+  private static final String CONTENT_INFO_TYPE = "ERMS";
+  private static final String OTHER_CONTENT_INFO_VALUE = "Test of other CITS";
+  private static final String EXPECTED_PARSE_EXCEPTION = "Expected ParseException";
+  private static final String MIXED = "MIXED";
 
+  /**
+   * Uses METS TYPE when TYPE is not OTHER.
+   */
   @Test
   public void contentTypeUsesType() throws ParseException {
-    Mets mets = new Mets();
-    MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
-    metsHdr.setOAISPACKAGETYPE("SIP");
+    final Mets mets = new Mets();
+    final MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
+    metsHdr.setOAISPACKAGETYPE(SIP_TYPE);
     mets.setMetsHdr(metsHdr);
 
-    mets.setTYPE("Text");
-    mets.setOTHERTYPE("Moving images - on tangible media");
+    mets.setTYPE(TYPE_TEXT);
+    mets.setOTHERTYPE(OTHER_TYPE_VALUE);
 
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     utils.setIPContentType(mets, sip);
 
-    Assert.assertEquals("Text", sip.getContentType().asString());
+    Assert.assertEquals(TYPE_TEXT, sip.getContentType().asString());
   }
 
+  /**
+   * Uses METS OTHERTYPE when TYPE is OTHER.
+   */
   @Test
   public void contentTypeUsesOtherType() throws ParseException {
-    Mets mets = new Mets();
-    MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
-    metsHdr.setOAISPACKAGETYPE("SIP");
+    final Mets mets = new Mets();
+    final MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
+    metsHdr.setOAISPACKAGETYPE(SIP_TYPE);
     mets.setMetsHdr(metsHdr);
 
-    mets.setTYPE("Other");
-    mets.setOTHERTYPE("Moving images - on tangible media");
+    mets.setTYPE(TYPE_OTHER);
+    mets.setOTHERTYPE(OTHER_TYPE_VALUE);
 
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     utils.setIPContentType(mets, sip);
 
-    Assert.assertEquals("Moving images - on tangible media", sip.getContentType().asString());
+    Assert.assertEquals(OTHER_TYPE_VALUE, sip.getContentType().asString());
   }
 
+  /**
+   * Uses CONTENTINFORMATIONTYPE when not OTHER.
+   */
   @Test
   public void contentInfoUsesType() throws ParseException {
-    final String CONTENT_INFORMATION_TYPE = "ERMS";
+    final Mets mets = new Mets();
+    mets.setCONTENTINFORMATIONTYPE(CONTENT_INFO_TYPE);
+    mets.setOTHERCONTENTINFORMATIONTYPE(OTHER_CONTENT_INFO_VALUE);
 
-    Mets mets = new Mets();
-    mets.setCONTENTINFORMATIONTYPE(CONTENT_INFORMATION_TYPE);
-    mets.setOTHERCONTENTINFORMATIONTYPE("Test of other CITS");
-
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     utils.setIPContentInformationType(mets, sip);
 
-    Assert.assertEquals(CONTENT_INFORMATION_TYPE, sip.getContentInformationType().asString());
+    Assert.assertEquals(CONTENT_INFO_TYPE, sip.getContentInformationType().asString());
   }
 
+  /**
+   * Uses OTHERCONTENTINFORMATIONTYPE when CONTENTINFORMATIONTYPE is OTHER.
+   */
   @Test
   public void contentInfoUsesOtherType() throws ParseException {
-    final String OTHER_CONTENT_INFORMATION_TYPE = "Test of other CITS";
+    final Mets mets = new Mets();
+    mets.setCONTENTINFORMATIONTYPE(TYPE_OTHER_UPPER);
+    mets.setOTHERCONTENTINFORMATIONTYPE(OTHER_CONTENT_INFO_VALUE);
 
-    Mets mets = new Mets();
-    mets.setCONTENTINFORMATIONTYPE("OTHER");
-    mets.setOTHERCONTENTINFORMATIONTYPE(OTHER_CONTENT_INFORMATION_TYPE);
-
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     utils.setIPContentInformationType(mets, sip);
 
-    Assert.assertEquals(OTHER_CONTENT_INFORMATION_TYPE, sip.getContentInformationType().asString());
+    Assert.assertEquals(OTHER_CONTENT_INFO_VALUE, sip.getContentInformationType().asString());
   }
 
+  /**
+   * Requires OTHERTYPE when TYPE is OTHER.
+   */
   @Test
   public void contentTypeOtherRequiresOtherType() {
-    Mets mets = new Mets();
-    MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
-    metsHdr.setOAISPACKAGETYPE("SIP");
+    final Mets mets = new Mets();
+    final MetsType.MetsHdr metsHdr = new MetsType.MetsHdr();
+    metsHdr.setOAISPACKAGETYPE(SIP_TYPE);
     mets.setMetsHdr(metsHdr);
 
-    mets.setTYPE("Other");
+    mets.setTYPE(TYPE_OTHER);
     mets.setOTHERTYPE("");
 
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     try {
       utils.setIPContentType(mets, sip);
-      Assert.fail("Expected ParseException");
-    } catch (ParseException e) {
+      Assert.fail(EXPECTED_PARSE_EXCEPTION);
+    } catch (final ParseException e) {
       Assert.assertTrue(e.getMessage().contains("OTHERTYPE"));
     }
   }
 
+  /**
+   * Requires OTHERCONTENTINFORMATIONTYPE when CONTENTINFORMATIONTYPE is OTHER.
+   */
   @Test
   public void contentInfoOtherRequiresOtherType() {
-    Mets mets = new Mets();
-    mets.setCONTENTINFORMATIONTYPE("OTHER");
+    final Mets mets = new Mets();
+    mets.setCONTENTINFORMATIONTYPE(TYPE_OTHER_UPPER);
     mets.setOTHERCONTENTINFORMATIONTYPE("");
 
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     try {
       utils.setIPContentInformationType(mets, sip);
-      Assert.fail("Expected ParseException");
-    } catch (ParseException e) {
+      Assert.fail(EXPECTED_PARSE_EXCEPTION);
+    } catch (final ParseException e) {
       Assert.assertTrue(e.getMessage().contains("OTHERCONTENTINFORMATIONTYPE"));
     }
   }
 
+  /**
+   * Leaves content information type default when blank.
+   */
   @Test
   public void contentInfoBlankKeepsDefault() throws ParseException {
-    Mets mets = new Mets();
+    final Mets mets = new Mets();
     mets.setCONTENTINFORMATIONTYPE("");
 
-    EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator("2.1.0"));
-    SIP sip = new EARKSIP();
+    final EARKUtils utils = new EARKUtils(new METSGeneratorFactory().getGenerator(SIP_VERSION));
+    final SIP sip = new EARKSIP();
 
     utils.setIPContentInformationType(mets, sip);
 
-    Assert.assertEquals("MIXED", sip.getContentInformationType().asString());
+    Assert.assertEquals(MIXED, sip.getContentInformationType().asString());
   }
 }
